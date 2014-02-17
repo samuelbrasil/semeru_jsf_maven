5628cd514398d3479269127b464ff5bff93dab80
Samuel <Samuel@192.168.1.3>
2014-02-17 00:38:09 -0300

    Criada novas classes FacesContextUtil e PhaseListenerUtil.

---
+++ semeru_jsf_maven/src/main/java/br/com/semeru/util/FacesContextUtil.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.com.semeru.util;
+
+import javax.faces.context.FacesContext;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author Samuel
+ */
+public class FacesContextUtil {
+    
+    private static final String HIBERNATE_SESSION = "hibernate_session";
+    
+    public static void setResquestSession(Session session){
+        FacesContext.getCurrentInstance().getExternalContext().getRequestMap().put(HIBERNATE_SESSION, session);
+                
+    }
+    
+   
+    public static Session getRequestSesssion() {
+        return (Session) FacesContext.getCurrentInstance().getExternalContext().getRequestMap().get(HIBERNATE_SESSION);
+    }
+    
+    
+    
+    
+}
